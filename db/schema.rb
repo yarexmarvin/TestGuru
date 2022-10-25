0f676d5182bc86c3fc571745a53d8a724b7551eb
Yarex Marvin <yarexmarvin@yahoo.com>
2022-10-25 10:04:28 +0400

    created initial data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_24_085343) do
+ActiveRecord::Schema.define(version: 2022_10_25_055643) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2022_10_24_085343) do
     t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "passed_tests", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_passed_tests_on_test_id"
+    t.index ["user_id"], name: "index_passed_tests_on_user_id"
   end
 
   create_table "questions", force: :cascade do |t|
@@ -56,6 +65,8 @@ ActiveRecord::Schema.define(version: 2022_10_24_085343) do
   end
 
   add_foreign_key "answers", "questions"
+  add_foreign_key "passed_tests", "tests"
+  add_foreign_key "passed_tests", "users"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
   add_foreign_key "tests", "user", column: "author_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,54 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = Category.create!([
+  { title: "Frontend" },
+  { title: "Backend" },
+  { title: "Mobile Development" },
+])
+
+tests = Test.create!([
+  { title: "HTML", level: 0, category_id: categories[0] },
+  { title: "CSS", level: 1, category_id: categories[0] },
+  { title: "JavaScript", level: 2, category_id: categories[0] },
+  { title: "Ruby", level: 3, category_id: categories[1] },
+  { title: "Ruby on Rails", level: 3, category_id: categories[1] },
+  { title: "Swift", level: 3, category_id: categories[2] },
+  { title: "Kotlin", level: 3, category_id: categories[2] },
+])
+
+questions = Question.create!([
+  { body: "How many types of heading does an HTML contain?", test_id: tests[0] },
+  { body: "What are the different types of Selectors in CSS?", test_id: tests[1] },
+  { body: " What are the different data types present in javascript?", test_id: tests[2] },
+  { body: "Difference between var and let keyword in javascript?", test_id: tests[2] },
+  { body: " Are class variables inherited? ", test_id: tests[3] },
+  { body: "What is ORM(Object-Relationship-Model)?", test_id: tests[4] },
+  { body: "What is typealias in Swift?", test_id: tests[5] },
+  { body: "How do you declare variables in Kotlin?", test_id: tests[5] },
+])
+
+answers = Answer.create!([
+  { body: "HTML defines six levels of headings", correct: true, question_id: questions[0] },
+  { body: "Element, Id , Class, Universal selectors only", correct: false, question_id: questions[1] },
+  { body: "JavaScript has 8 types: undefined, null, boolean, number, bigint, string, symbol, and object", correct: true, question_id: questions[2] },
+  { body: "There is no differences between var and let", correct: false, question_id: questions[3] },
+  { body: "No. The behavior is different than inheritance. Any alteration of a class variable by a subclass affects that class variable in the superclass and all other subclasses of the superclass.", correct: true, question_id: questions[4] },
+  { body: "Object–relational mapping in computer science is a programming technique for converting data between type systems using object-oriented programming languages. ", correct: true, question_id: questions[5] },
+  { body: "In Swift, typealias is a class that gives a new name, or an alias, to an existing type.", correct: false, question_id: questions[6] },
+  { body: "Kotlin uses two different keywords to declare variables: ral and var ", correct: false, question_id: questions[7] },
+])
+
+users = User.create!([
+    {name: 'Mike', login: 'mike77', password: 'cool-mike-77'},
+    {name: 'Brian', login: 'brian1', password: 'brian1-1-1'},
+    {name: 'Kate', login: 'kate90', password: 'kate-brown1990', type: 'admin'},
+])
+
+passed_tests = PassedTest.create!([
+    {user_id: users[0], test_id: tests[1]},
+    {user_id: users[1], test_id: tests[3]},
+    {user_id: users[0], test_id: tests[2]},
+    {user_id: users[1], test_id: tests[4]},
+])
