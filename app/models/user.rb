37e9fa85dd107db6cb31f8b345278083cbcb751b
Yarex Marvin <yarexmarvin@yahoo.com>
2022-10-26 20:50:13 +0400

    fixes after review

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
-
-  has_many :created_tests, class_name: "Test", foreign_key: :author_id
-
-  has_many :passed_tests
-  has_many :tests, through: :passed_tests
+  has_many :created_tests, class_name: "Test", foreign_key: :author_id, dependent: :destroy
+  has_many :passed_tests, dependent: :destroy
+  has_many :tests, through: :passed_tests, dependent: :destroy
 
   def passed_tests_by_level(level)
-    Test.joins("INNER JOIN passed_tests ON tests.id = passed_tests.test_id ")
-      .where(passed_tests: { user_id: id }, level: level);
+    Test.joins(:passed_tests)
+      .where(passed_tests: { user_id: id }, level: level)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,40 +20,40 @@ users = User.create!([
   ])
 
 tests = Test.create!([
-  { title: "HTML", level: 0, category_id: categories[0].id, author_id: users[2].id },
-  { title: "CSS", level: 1, category_id: categories[0].id, author_id: users[2].id },
-  { title: "JavaScript", level: 2, category_id: categories[0].id, author_id: users[2].id },
-  { title: "Ruby", level: 3, category_id: categories[1].id, author_id: users[2].id },
-  { title: "Ruby on Rails", level: 3, category_id: categories[1].id, author_id: users[2].id },
-  { title: "Swift", level: 3, category_id: categories[2].id, author_id: users[2].id },
-  { title: "Kotlin", level: 3, category_id: categories[2].id, author_id: users[2].id },
+  { title: "HTML", level: 0, category_id: categories[0], author_id: users[2] },
+  { title: "CSS", level: 1, category_id: categories[0], author_id: users[2] },
+  { title: "JavaScript", level: 2, category_id: categories[0], author_id: users[2] },
+  { title: "Ruby", level: 3, category_id: categories[1], author_id: users[2] },
+  { title: "Ruby on Rails", level: 3, category_id: categories[1], author_id: users[2] },
+  { title: "Swift", level: 3, category_id: categories[2], author_id: users[2] },
+  { title: "Kotlin", level: 3, category_id: categories[2], author_id: users[2] },
 ])
 
 questions = Question.create!([
-  { body: "How many types of heading does an HTML contain?", test_id: tests[0].id },
-  { body: "What are the different types of Selectors in CSS?", test_id: tests[1].id },
-  { body: " What are the different data types present in javascript?", test_id: tests[2].id },
-  { body: "Difference between var and let keyword in javascript?", test_id: tests[2].id },
-  { body: " Are class variables inherited? ", test_id: tests[3].id },
-  { body: "What is ORM(Object-Relationship-Model)?", test_id: tests[4].id },
-  { body: "What is typealias in Swift?", test_id: tests[5].id },
-  { body: "How do you declare variables in Kotlin?", test_id: tests[5].id },
+  { body: "How many types of heading does an HTML contain?", test_id: tests[0] },
+  { body: "What are the different types of Selectors in CSS?", test_id: tests[1] },
+  { body: " What are the different data types present in javascript?", test_id: tests[2] },
+  { body: "Difference between var and let keyword in javascript?", test_id: tests[2] },
+  { body: " Are class variables inherited? ", test_id: tests[3] },
+  { body: "What is ORM(Object-Relationship-Model)?", test_id: tests[4] },
+  { body: "What is typealias in Swift?", test_id: tests[5] },
+  { body: "How do you declare variables in Kotlin?", test_id: tests[5] },
 ])
 
 answers = Answer.create!([
-  { body: "HTML defines six levels of headings", correct: true, question_id: questions[0].id },
-  { body: "Element, Id , Class, Universal selectors only", correct: false, question_id: questions[1].id },
-  { body: "JavaScript has 8 types: undefined, null, boolean, number, bigint, string, symbol, and object", correct: true, question_id: questions[2].id },
-  { body: "There is no differences between var and let", correct: false, question_id: questions[3].id },
-  { body: "No. The behavior is different than inheritance. Any alteration of a class variable by a subclass affects that class variable in the superclass and all other subclasses of the superclass.", correct: true, question_id: questions[4].id },
-  { body: "Object–relational mapping in computer science is a programming technique for converting data between type systems using object-oriented programming languages. ", correct: true, question_id: questions[5].id},
-  { body: "In Swift, typealias is a class that gives a new name, or an alias, to an existing type.", correct: false, question_id: questions[6].id },
-  { body: "Kotlin uses two different keywords to declare variables: ral and var ", correct: false, question_id: questions[7].id },
+  { body: "HTML defines six levels of headings", correct: true, question_id: questions[0] },
+  { body: "Element, Id , Class, Universal selectors only", correct: false, question_id: questions[1] },
+  { body: "JavaScript has 8 types: undefined, null, boolean, number, bigint, string, symbol, and object", correct: true, question_id: questions[2] },
+  { body: "There is no differences between var and let", correct: false, question_id: questions[3] },
+  { body: "No. The behavior is different than inheritance. Any alteration of a class variable by a subclass affects that class variable in the superclass and all other subclasses of the superclass.", correct: true, question_id: questions[4] },
+  { body: "Object–relational mapping in computer science is a programming technique for converting data between type systems using object-oriented programming languages. ", correct: true, question_id: questions[5]},
+  { body: "In Swift, typealias is a class that gives a new name, or an alias, to an existing type.", correct: false, question_id: questions[6] },
+  { body: "Kotlin uses two different keywords to declare variables: ral and var ", correct: false, question_id: questions[7] },
 ])
 
 passed_tests = PassedTest.create!([
-  { user_id: users[0].id, test_id: tests[1].id },
-  { user_id: users[1].id, test_id: tests[3].id },
-  { user_id: users[0].id, test_id: tests[2].id },
-  { user_id: users[1].id, test_id: tests[4].id },
+  { user_id: users[0], test_id: tests[1] },
+  { user_id: users[1], test_id: tests[3] },
+  { user_id: users[0], test_id: tests[2] },
+  { user_id: users[1], test_id: tests[4] },
 ])
