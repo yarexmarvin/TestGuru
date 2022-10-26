c702d9a8943e017eef07b41c5da2f54d1bbd32f4
Yarex Marvin <yarexmarvin@yahoo.com>
2022-10-26 11:45:11 +0400

    added associations

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: "User", foreign_key: :author_id
+  has_many :questions
+  has_many :passed_tests
+  has_many :users, through: :passed_tests
+  
   def self.test_by_category(category)
     Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
       .where(categories: { title: category })
