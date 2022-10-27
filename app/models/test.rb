99623c02f08867a29cc3f12b2ec987c5f9eec721
Yarex Marvin <yarexmarvin@yahoo.com>
2022-10-27 19:03:00 +0400

    fixes after review

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,7 +3,7 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: "User", foreign_key: :author_id
 
   has_many :questions, dependent: :destroy
-  has_many :passed_tests, dependent: :destroy
+  has_many :passed_tests
   has_many :users, through: :passed_tests, dependent: :destroy
 
   def self.test_by_category(category)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :created_tests, class_name: "Test", foreign_key: :author_id, dependent: :destroy
-  has_many :passed_tests, dependent: :destroy
+  has_many :passed_tests
   has_many :tests, through: :passed_tests, dependent: :destroy
 
   def passed_tests_by_level(level)
