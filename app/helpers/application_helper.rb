1184e1a7131226ae4897b6bb3e69a1a6ea7659a4
Yarex Marvin <yarexmarvin@yahoo.com>
2022-10-30 18:12:20 +0400

    refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def github_url(author, repo)
-    link_to 'Test Guru project', "https://github.com/#{author}/#{repo}", target: "_blank"
+    link_to "Projects's repository", "https://github.com/#{author}/#{repo}", target: "_blank"
   end
 
   def current_year
--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -1,5 +1,18 @@
 module TestsHelper
-    def questions_amount(test)
-        test.questions.count
-    end
+  LEVELS = {
+    0 => :trainee,
+    1 => :beginner,
+    2 => :intermediate,
+    3 => :difficult,
+    4 => :hard,
+    5 => :extreme,
+  }
+
+  def questions_amount(test)
+    test.questions.count
+  end
+
+  def level_translation(level)
+    LEVELS[level]
+  end
 end
