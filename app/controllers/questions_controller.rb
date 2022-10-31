0663815f0365bbf24e0eb4b036a9998b13373b0c
Yarex Marvin <yarexmarvin@yahoo.com>
2022-10-31 18:02:45 +0400

    fixed router

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,11 +1,8 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[ new create]
-  before_action :find_question, only: %i[ index show edit update destroy ]
+  before_action :find_test, only: %i[new create]
+  before_action :find_question, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
-
-  def index;
-  end
 
   def show; end
 
