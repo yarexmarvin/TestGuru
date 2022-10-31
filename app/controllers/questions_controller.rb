2afeea7d39b5ffc12d466dfb038b821a7d70ed72
Yarex Marvin <yarexmarvin@yahoo.com>
2022-10-31 18:08:07 +0400

    added redirect after destroying question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -33,7 +33,7 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    render plain: "The question has been deleted!"
+    redirect_to @question.test
   end
 
   def find_question
