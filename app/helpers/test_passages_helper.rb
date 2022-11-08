0ebd9a29db15dc43c606b6abafb7060930cad3f9
Yarex Marvin <yarexmarvin@yahoo.com>
2022-11-08 22:21:52 +0400

    test_passages localization

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -2,14 +2,14 @@ module TestPassagesHelper
 
   def define_result
     if @test_passage.passed?
-      content_tag(:p, "Test passed", class: "test_passed")
+      content_tag(:p,  t('.test_passed'), class: "test_passed")
     else
-      content_tag(:p, "Test not passed", class: "test_not_passed")
+      content_tag(:p,  t('.test_not_passed'), class: "test_not_passed")
     end
   end
 
   def question_number(question)
     question_number = @test_passage.test.questions.index(question) + 1
-    content_tag(:h4, "Question № #{question_number}")
+    content_tag(:h4, t('.question_title', question_number: question_number))
   end
 end
