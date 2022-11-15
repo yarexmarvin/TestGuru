01984abe46ade86069fff6d3e754467e404b62b7
Yarex Marvin <yarexmarvin@yahoo.com>
2022-11-15 22:51:35 +0400

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   # config.action_mailer.perform_caching = false
-  # config.action_mailer.default_url_options = { host: "" }
+  config.action_mailer.default_url_options = { host: "sleepy-ridge-38961.herokuapp.com" }
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   address: "smtp.gmail.com",
