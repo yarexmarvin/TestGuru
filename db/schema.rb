9a31ed0117389e40754263bed3e9767aaee18842
Yarex Marvin <yarexmarvin@yahoo.com>
2022-11-15 21:47:32 +0400

    deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_11_11_153038) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
