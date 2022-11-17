daa129ab84ad0ee1fcc2b3caffadef027b756b98
Yarex Marvin <yarexmarvin@yahoo.com>
2022-11-17 18:11:20 +0400

    feedback fixes

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,7 +1,5 @@
 class FeedbacksController < ApplicationController
   def create
-    p "======"
-    p params[:message]
     unless params[:message].nil?
       FeedbackMailer.send_feedback(current_user, params[:message]).deliver_now
       redirect_to root_path, { notice: "Сообщение отправлено" }
